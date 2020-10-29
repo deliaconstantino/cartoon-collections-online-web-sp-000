1e6064c54a05bc95550885cf23b6a79cda9d6744
Delia Constantino <dconstan92@gmail.com>
2020-10-29 17:00:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,4 @@
-def roll_call_dwarves(dwarves)
+odds_and_evens = [1, 3, 2, 18, 5, 10, 24]def roll_call_dwarves(dwarves)
   dwarves.each_with_index{ |name, index| puts "#{index + 1}. #{name}"}
 end
 
@@ -11,11 +11,14 @@ def long_planeteer_calls(calls_array)
 end
 
 def find_the_cheese(food)
-  food.each do |food_type|
-    cheese_types = ["cheddar", "gouda", "camembert"]
-    if cheese_types.include?(food_type)
-      return food_type
-    end
-  end
-  nil
 end
+
+# def find_the_cheese(food)
+#   food.each do |food_type|
+#     cheese_types = ["cheddar", "gouda", "camembert"]
+#     if cheese_types.include?(food_type)
+#       return food_type
+#     end
+#   end
+#   nil
+# end
